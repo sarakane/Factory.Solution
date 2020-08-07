ffd6f7233228f04c97f81b7ac3a61c255e46b240
Sara Kane <kane.sarak@gmail.com>
2020-08-07 09:50:47 -0700

    Add home index and controller and engineer index and controller

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,24 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Engineers.ToList());
+    }
+  }
+}
