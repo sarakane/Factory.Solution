41458d8a884e2ea2204ba9d6b33bbd4ecdde029b
Sara Kane <kane.sarak@gmail.com>
2020-08-07 16:07:49 -0700

    Add ViewBags for engineers and machines to home controller to show lists of engineers and machines on the home index

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,11 +1,23 @@
+using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
     public ActionResult Index()
     {
+      ViewBag.Engineers = _db.Engineers.ToList();
+      ViewBag.Machines = _db.Machines.ToList();
       return View();
     }
   }
