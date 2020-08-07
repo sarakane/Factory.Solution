306d093cf3aa4bbe55800ae18a976356b0836f1d
Sara Kane <kane.sarak@gmail.com>
2020-08-07 12:15:56 -0700

    Add Details machine page and Details GET method in MachinesController

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -7,31 +7,40 @@ using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class MachinesController : Controller
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
     {
-      private readonly FactoryContext _db;
-
-      public MachinesController(FactoryContext db)
-      {
-        _db = db;
-      }
-
-      public ActionResult Index()
-      {
-        return View(_db.Machines.ToList());
-      }
-
-      public ActionResult Create()
-      {
-        return View();
-      }
-
-      [HttpPost]
-      public ActionResult Create(Machine machine)
-      {
-        _db.Machines.Add(machine);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-      }
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisMachine = _db.Machines
+      .Include(machine => machine.Engineers)
+      .ThenInclude(join => join.Engineer)
+      .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+  }
 }
