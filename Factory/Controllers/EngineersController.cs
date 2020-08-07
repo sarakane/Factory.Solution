9ab65cab8808b9ce52ea146d3ba89b76e5a84c7f
Sara Kane <kane.sarak@gmail.com>
2020-08-07 10:16:02 -0700

    Add Details engineer page and Details GET method in EngineersController

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -33,5 +33,14 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+      .Include(engineer => engineer.Machines)
+      .ThenInclude(join => join.Machine)
+      .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
   }
 }
